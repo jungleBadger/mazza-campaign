d52828eab0d6fdc3dcb28b72f1ffb68d21cfe1d0
Daniel Abrao <danielpaladar@gmail.com>
2018-01-21 20:05:58 -0200

    Add home panel component

---
+++ client/admin_module/js/components/home-panel.vue
@@ -0,0 +1,27 @@
+<template>
+	<transition
+		mode="out-in"
+		enter-active-class="animated fadeInDown"
+		leave-active-class="animated fadeOutUp">
+		<div>
+			Home
+		</div>
+	</transition>
+</template>
+
+<script type="text/javascript">
+	(function () {
+		"use strict";
+		module.exports = {
+			"name": "HomePanel",
+			"props": {},
+			"data": function () {
+				return {}
+			},
+			"components": {},
+			"methods": {}
+		};
+	}());
+</script>
+<style scoped>
+</style>
