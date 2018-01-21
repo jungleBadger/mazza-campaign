2cafdaa4c87dc74bbc78a3a91d0027d9232c4bef
Daniel Abrao <danielpaladar@gmail.com>
2018-01-21 16:28:05 -0200

    Add first campaignUsers APIs

---
+++ server/routes/partials/campaignUsersHandler.js
@@ -0,0 +1,23 @@
+(function () {
+	"use strict";
+
+	const users = new require("../../helpers/users")();
+
+	module.exports = function (app) {
+		app.post("/createCampaignUser", function (req, res) {
+			users.createCampaignUser(req.body.user).then((result) => {
+				return res.status(200).send(result);
+			}).catch((err) => {
+				return res.status(err.status || 500).send(err.message || err);
+			});
+		});
+
+		app.get("/queryCampaignUser", function (req, res) {
+			users.querySingleUser(req.query.userEmail).then((result) => {
+				return res.status(200).send(result);
+			}).catch((err) => {
+				return res.status(err.status || 500).send(err.message || err);
+			});
+		});
+	}
+}());
