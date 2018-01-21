6d77a887668566342035127f8500da92bb2b2fb0
Daniel Abrao <danielpaladar@gmail.com>
2018-01-21 17:07:03 -0200

    Add fontawesome import to test on heroku

--- client/admin_module/js/components/foo.vue
+++ client/admin_module/js/components/foo.vue
@@ -1,12 +1,12 @@
 <template>
 	<transition
 		mode="out-in"
-
 		enter-active-class="animated fadeInDown"
 		leave-active-class="animated fadeOutUp">
 	>
 		<div>
 			affa
+			<font-awesome-icon :icon="spinner" spin></font-awesome-icon>
 		</div>
 	</transition>
 
@@ -15,12 +15,18 @@
 <script type="text/javascript">
     (function () {
         "use strict";
+        const spinnerIcon = require("@fortawesome/fontawesome-pro-light/faSpinner");
         module.exports = {
             "name": "Foo",
             "props": {},
             "data": function () {
                 return {}
             },
+			"computed": {
+				spinner () {
+					return spinnerIcon;
+				}
+			},
             "components": {},
             "methods": {}
         };
