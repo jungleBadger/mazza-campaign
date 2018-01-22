1d43a4b393f7628f2bd1dcf2d8e0b3fbbf48e7ff
Daniel Abrao <danielpaladar@gmail.com>
2018-01-21 22:40:22 -0200

    Add logout option on bottom of sidemenu

--- client/admin_module/js/components/app-sidemenu.vue
+++ client/admin_module/js/components/app-sidemenu.vue
@@ -1,25 +1,36 @@
-<template>
-    <aside id="sidemenu" class="menu">
+<template><aside id="sidemenu" class="menu">
+	<div
+		class="nav-wrapper"
+		role="navigation"
+		aria-label="main navigation">
 		<div
 			class="menu-item"
-			v-for="(groupRoutes, group) in routes">
+			v-for="(groupRoutes, group) in groupedRoutes">
 			<template v-if="groupRoutes.length">
 				<p class="menu-label">
 					{{group}}
 				</p>
 			</template>
-
 			<div class="menu-list">
 				<router-link
 					v-for="route in groupRoutes"
 					:to="route.path"
 					:key="route.name"
-					:class="{'is-active':selectedRoute === route.name}">
+					:class="{'is-active': selectedRoute === route.name}">
 					<font-awesome-icon :icon="route.icon" />
-					<span>{{route.displayName}}</span>
+					<span class="route-name">{{route.displayName}}</span>
 				</router-link>
 			</div>
 		</div>
+	</div>
+	<div class="menu-item">
+		<a class="button is-danger">
+			<span class="icon">
+				<font-awesome-icon :icon="logoutIcon" />
+			</span>
+			<span>Logout</span>
+		</a>
+	</div>
 	</aside>
 </template>
 
@@ -33,30 +44,14 @@
                 return {}
             },
 			"computed": {
-				"routes": function () {
-					let routesDictionary = {};
-					this.$router.options.routes.forEach((route) => {
-						if (!route.default) {
-							if (route.group) {
-								if (routesDictionary[route.group]) {
-									routesDictionary[route.group].push(route);
-								} else {
-									routesDictionary[route.group] = [route];
-								}
-							} else {
-								if (routesDictionary.other) {
-									routesDictionary.other.push(route);
-								} else {
-									routesDictionary.other = [route];
-								}
-							}
-						}
-
-					});
-					return routesDictionary;
+				"groupedRoutes": function () {
+					return this.$store.getters["routes/getGroupedRoutes"];
 				},
 				"selectedRoute": function () {
 					return this.$route.name;
+				},
+				"logoutIcon": function () {
+					return require("@fortawesome/fontawesome-pro-light/faSignOut");
 				}
 			},
             "components": {},
@@ -65,14 +60,34 @@
     }());
 </script>
 <style scoped lang="scss" rel="stylesheet/scss">
+	$shadow-color: #e9e9e9;
 	#sidemenu {
 		height: calc(100% - 60px);
-		width: 15%;
+		min-width: 100px;
+		flex: 2;
 		display: flex;
 		flex-direction: column;
+		user-select: none;
+		z-index: 1;
+		background-color: #EDF2F5;
+		box-shadow: 0 2px 5px 1px $shadow-color;
+		@media screen and (max-width: 1024px) {
+			display: none;
+		}
+	}
+
+	.nav-wrapper {
+		flex: 1;
 	}
 
 	.menu-item {
 		margin: 10px 5px;
 	}
+
+	.menu-label {
+		font-weight: 600;
+	}
+
+
+
 </style>
