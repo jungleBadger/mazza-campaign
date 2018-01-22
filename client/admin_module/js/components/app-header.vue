0fdf81f343ac4a0ec847a6a609352164c3685c2c
Daniel Abrao <danielpaladar@gmail.com>
2018-01-21 22:40:08 -0200

    Add responsive/alternative navigation menu

--- client/admin_module/js/components/app-header.vue
+++ client/admin_module/js/components/app-header.vue
@@ -1,38 +1,121 @@
 <template>
-    <nav id="header" class="navbar is-dark" role="navigation" aria-label="main navigation">
+	<nav id="header" class="navbar is-dark" role="navigation" aria-label="alternative navigation">
 		<div class="navbar-brand">
-			<a class="navbar-item" href="https://bulma.io">
-				<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+			<a href="/admin" class="navbar-item">
+				<img data-v-96b41898="" src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
 			</a>
-
-			<button class="button navbar-burger">
+			<div
+				class="navbar-burger burger"
+				@click="toggleMenu"
+				:class="{'is-active': menuOpen}"
+			>
 				<span></span>
 				<span></span>
 				<span></span>
-			</button>
+			</div>
 		</div>
-
-    </nav>
+		<div
+			id="navbarExampleTransparentExample"
+			class="navbar-menu"
+			:class="{'is-active': menuOpen}">
+			<div class="navbar-start">
+				<div
+					role="navigation" aria-label="main navigation"
+					class="navbar-item"
+					@click="toggleMenu"
+					v-for="(groupRoutes, group) in groupedRoutes">
+					<template v-if="groupRoutes.length">
+						<p class="menu-label">
+							{{group}}
+						</p>
+					</template>
+					<router-link
+						v-for="route in groupRoutes"
+						:to="route.path"
+						:key="route.name"
+						class="navbar-link"
+						:class="{'is-active': selectedRoute === route.name}">
+						<font-awesome-icon :icon="route.icon" />
+						<span class="route-name">
+							{{route.displayName}}
+						</span>
+					</router-link>
+				</div>
+			</div>
+			<div class="navbar-end">
+				<div class="navbar-dropdown">
+					<hr class="navbar-divider">
+					<div class="navbar-item">
+						<a class="button is-danger">
+							<span class="icon">
+								<font-awesome-icon :icon="logoutIcon" />
+							</span>
+							<span>Logout</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
 </template>
 
 <script type="text/javascript">
-    (function () {
-        "use strict";
-        module.exports = {
-            "name": "AppHeader",
-            "props": {},
-            "data": function () {
-                return {}
-            },
-            "components": {},
-            "methods": {}
-        };
-    }());
+	(function () {
+		"use strict";
+		module.exports = {
+			"name": "AppHeader",
+			"props": {},
+			"data": function () {
+				return {
+					"menuOpen": false
+				}
+			},
+			"computed": {
+				"groupedRoutes": function () {
+					return this.$store.getters["routes/getGroupedRoutes"];
+				},
+				"selectedRoute": function () {
+					return this.$route.name;
+				},
+				"menuIcon": function () {
+					return require("@fortawesome/fontawesome-pro-light/faBars");
+				},
+				"logoutIcon": function () {
+					return require("@fortawesome/fontawesome-pro-light/faSignOut");
+				}
+			},
+			"components": {},
+			"methods": {
+				"toggleMenu": function () {
+					this.menuOpen = !this.menuOpen;
+				}
+			}
+		};
+	}());
 </script>
 <style scoped>
 	#header {
 		width: 100%;
+		z-index: 5;
 		height: 60px;
 		box-sizing: border-box;
 	}
+
+	.navbar-brand {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+	}
+
+	/*.navbar-brand> .logo-area {*/
+		/*height: 100%;*/
+		/*display: flex;*/
+		/*justify-content: center;*/
+		/*align-items: center;*/
+		/*padding: 0 10px;*/
+	/*}*/
+
 </style>
