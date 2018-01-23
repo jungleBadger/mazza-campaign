1ca42ebfa8729fa82f3ccdde06ab67530be173ec
Daniel Abrao <danielpaladar@gmail.com>
2018-01-22 23:54:34 -0200

    Add test environment mongo credentials support

--- server/configs/mongo-string.js
+++ server/configs/mongo-string.js
@@ -1,6 +1,8 @@
 (function (process) {
     "use strict";
 
-    module.exports = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_AUTH_SOURCE}`;
+    module.exports = process.env.TEST_ENV || process.env === "TEST" ?
+		`mongodb://${process.env.MONGO_TEST_USER}:${process.env.MONGO_TEST_PASSWORD}@${process.env.MONGO_TEST_URL}:${process.env.MONGO_TEST_PORT}/${process.env.MONGO_TEST_AUTH_SOURCE}` :
+		`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_AUTH_SOURCE}`;
 
 }(process));
