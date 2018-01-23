6d37f0b1e0185dced19eb447f315ddc91253d443
Daniel Abrao <danielpaladar@gmail.com>
2018-01-23 00:05:07 -0200

    Fix campaigns collection reference

--- server/helpers/campaigns.js
+++ server/helpers/campaigns.js
@@ -2,8 +2,8 @@
 	"use strict";
 
 	const mongo = new require("./mongo")();
-	// const createError = require("http-errors");
-	const CAMPAIGNS_COLLECTION = "campaings";
+	const createError = require("http-errors");
+	const CAMPAIGNS_COLLECTION = "campaigns";
 	const Campaign = require("../model/Campaign");
 
 	module.exports = function () {
@@ -16,6 +16,51 @@
 							instance,
 							CAMPAIGNS_COLLECTION,
 							newCampaign
+						).then((result) => {
+							resolve(result);
+						}).catch((err) => {
+							reject(err);
+						});
+					}).catch((err) => {
+						reject(err);
+					});
+				});
+			},
+			"queryCampaign": function (params) {
+				return new Promise((resolve, reject) => {
+					if (!params || !params.query || typeof params.query !== "object") {
+						return reject(createError(400, "Invalid params to query campaign"));
+					}
+					if (params.query._id) {
+						params.query._id = mongo.ObjectId(params.query._id);
+					}
+					mongo.connect().then((instance) => {
+						mongo.findOne(
+							instance,
+							CAMPAIGNS_COLLECTION, {
+								"query": params.query,
+								"projection": params.projection
+							}
+						).then((result) => {
+							resolve(result);
+						}).catch((err) => {
+							reject(err);
+						});
+					}).catch((err) => {
+						reject(err);
+					});
+				});
+			},
+			"deleteCampaign": function (campaignId) {
+				return new Promise((resolve, reject) => {
+					if (!campaignId) {
+						return reject(createError(400, "Invalid params to delete campaign"));
+					}
+					mongo.connect().then((instance) => {
+						mongo.deleteOneById(
+							instance,
+							CAMPAIGNS_COLLECTION,
+							campaignId
 						).then((result) => {
 							resolve(result);
 						}).catch((err) => {
